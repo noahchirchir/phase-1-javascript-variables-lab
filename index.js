c6d3e92f1a6d81d87f97376d611179b060efb0d4
Noah chirchir <chirchirnoah37@gmail.com>
2025-10-03 09:14:46 +0300

    Complete Lab

--- index.js
+++ index.js
@@ -1,6 +1,1 @@
 // Code your solution in this file!
-// let companyName ='Scuber'
-const companyName='Scuber';
-let mostProfitableNeighborhood='Chelsea'
-let companyCeo='Susan Smith'
-expect (companyCeo).to.equal('Susan Smith')
